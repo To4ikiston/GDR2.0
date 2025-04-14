58f05abcaa7cd31ca7e1083e32a2040de1af73b7
To4ikiston <ov4innikovyan@gmail.com>
2025-04-14 22:17:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -98,5 +98,6 @@ async def root():
 
 if __name__ == "__main__":
     import os
+    import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
