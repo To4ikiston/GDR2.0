60da7b502cce690e9fd4f5b91b947b341c973923
To4ikiston <ov4innikovyan@gmail.com>
2025-04-15 16:34:33 +0500

    Update worklet-processor.js

--- static/worklet-processor.js
+++ static/worklet-processor.js
@@ -2,7 +2,6 @@ class PCMWriterProcessor extends AudioWorkletProcessor {
   process(inputs, outputs, parameters) {
     const input = inputs[0][0];
     if (input) {
-      // float32 -> int16
       const int16data = new Int16Array(input.length);
       for (let i = 0; i < input.length; i++) {
         let val = input[i] * 32768;
